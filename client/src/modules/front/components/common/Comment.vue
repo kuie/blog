6f5830fa4e0d1df4d36fd4694a339ace215452c2
田志鹏 <tianzhipeng@zhongchuangsanyou.com>
2019-12-23 14:03:00 +0800

    提交 文件

---
+++ client/src/modules/front/components/common/Comment.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="comment-box">
+        <div id="disqus_thread"></div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'comment-box',
+    props: {
+        sourceId: {
+            type: String,
+            required: true,
+        },
+    },
+    mounted() {
+        if (typeof DISQUS !== 'undefined') {
+            DISQUS.reset({
+                reload: true,
+                config: function () {
+                    this.page.identifier = this.sourceId;
+                    this.page.url = location.href;
+                },
+            });
+        }
+    },
+};
+</script>
+
+<style lang="stylus" scoped>
+  .comment-box
+    min-height 80px
+    margin-bottom 20px
+</style>
